5f3cf4efba79191c05a16cbc29bc702f3f47fa5a
nmgs <nuno.serrador@xpand-it.com>
2021-12-06 15:37:39 +0000

    final async crud

---
+++ XpandDEVWebCourse.API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using XpandDEVWebCourse.Business;
+
+
+
+namespace XpandDEVWebCourse.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ICarsService _carsService;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, ICarsService carsService)
+        {
+            _logger = logger;
+            _carsService = carsService;
+        }
+
+       
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        
+        [HttpDelete]
+        [Route("api_DeleteCar")]
+        public bool DeleteCar(int id)
+        {
+            try
+            {
+                _carsService.DeleteCarAsync(id);
+                return true;
+            }
+            catch 
+            {
+                return false;
+            }
+
+        }
+    }
+}
---
+++ XpandDEVWebCourse/Controllers/api/CarsApiController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using XpandDEVWebCourse.Business;
+using XpandDEVWebCourse.Web.ViewModels;
+using XpandDEVWebCourse.Models;
+
+namespace XpandDEVWebCourse.Web.Controllers.api
+{
+    [ApiController]
+    [Route("api/cars")]
+    public class CarsApiController : Controller
+    {
+        private readonly ICarsService _carsService;
+
+        public CarsApiController(ICarsService carsService)
+        {
+            _carsService = carsService;
+        }
+
+        [HttpGet]
+        [Route("DeleteCar")]
+        public async Task<IActionResult> DeleteCars(int id)
+        {
+
+            var cars = await _carsService.DeleteCarAsync(id);
+
+            var result = _carsService.GetAllCarsAsync();
+
+            // return PartialView("_ListedCar", result);
+            if (cars.IsSuccess)
+                return Ok();
+            return BadRequest();
+
+        }
+
+        [HttpPost]
+        [Route("NewCar")]
+        public async Task<IActionResult> NewCars(CarViewModel car)
+        {
+            var carDto = new Car()
+            {
+                Model = car.Model,
+                ExternalId = car.ExternalId,
+                NrBolts = car.NrBolts
+            };
+
+            var cars = await _carsService.NewCarAsync(carDto);
+
+            var result = await _carsService.GetAllCarsAsync();
+
+            //return PartialView("_ListedCar", result);
+            if (cars.IsSuccess)
+                return Ok();
+            return BadRequest();
+        }
+
+        [HttpGet]
+        [Route("UpdateCar")]
+        public async Task<IActionResult> UpdateCars(int id, string model, int nrBolts, int externalId)
+        {
+            var carDto = new Car()
+            {
+                Id = id,
+                Model = model,
+                ExternalId = externalId,
+                NrBolts = nrBolts
+            };
+
+            var cars = await _carsService.UpdateCarAsync(carDto);
+            //var result = await _carsService.GetAllCarsAsync();
+            //return PartialView("_ListedCar", result);
+            if (cars.IsSuccess)
+                return Ok();
+            return BadRequest();
+        }
+        }
+}
